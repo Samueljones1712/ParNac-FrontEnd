a2521805a3d5b2a012eb1125a972803a9ff7a772
Samuel-Jones2001 <samuel.jones@ucr.ac.cr>
2023-05-27 02:20:48 -0600

    26-05-23

--- src/app/components/entrada/entrada.component.ts
+++ src/app/components/entrada/entrada.component.ts
@@ -7,6 +7,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ToastrService } from 'ngx-toastr';
 import { ParkService } from 'src/app/services/park.service';
 import { parkNational } from 'src/app/interface/parkNational';
+import { Administrador, UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interface/user';
 
 @Component({
   selector: 'app-entrada',
@@ -19,12 +21,19 @@ export class EntradaComponent implements OnInit {
   dtTrigger: Subject<any> = new Subject<any>();
 
   loading: boolean = false;
+  loadingParks: boolean = false;
+  loadingCedula: boolean = false;
 
   listEntradas: Entrada[] = []
+  listAdmins: Administrador[] = [];
 
   id: string = "";
   fk_idParque: String = "";
+  nombreParque: String = "";
+  nombreCedula: String = "";
+  cedula: String = "";
   tarifa: String = "";
+  valueDelete: string = "";
 
   listParks: parkNational[] = [];
 
@@ -40,34 +49,16 @@ export class EntradaComponent implements OnInit {
     tarifa: ""
   }
 
-  constructor(private entradaService: EntradaService, private router: Router,
-    private toastr: ToastrService, private parkService: ParkService) { }
 
-  savePark(e: any): void {
-    let name = e.target.value;
-
-    let list = this.listParks.filter(x => x.Nombre === name)[0];
-
-
-    this.tarifa = list.Tarifa_Nacionales_colones;
-
-    this.fk_idParque = list.Id;
-  }
-
-  loadForm() {
-    this.entradaForm = new FormGroup({
-      id: new FormControl(this.entrada.id),
-      fecha: new FormControl(this.entrada.fecha, [Validators.required]),
-      fk_cedula: new FormControl(this.entrada.fk_cedula, [Validators.required]),
-      estado: new FormControl(this.entrada.estado, [Validators.required]),
-      fechaVencimiento: new FormControl(this.entrada.fechaVencimiento, [Validators.required])
-    });
-  }
 
   ngOnInit(): void {
+
+
     this.loadForm();
     this.getEntrada();
+
     this.loadParkes();
+
 
     this.dtOptions = {
       pagingType: 'full_numbers',
@@ -80,9 +71,79 @@ export class EntradaComponent implements OnInit {
     }
   }
 
+  constructor(private entradaService: EntradaService, private router: Router,
+    private toastr: ToastrService, private parkService: ParkService, private userService: UserService) { }
+
+  savePark(e: any): void {
+    let name = e.target.value;
+
+    let list = this.listParks.filter(x => x.Nombre === name)[0];
+
+    //this.toastr.success("Has seleccionado el Parque:" + list.Nombre, "Correcto");
+
+    this.tarifa = list.Tarifa_Nacionales_colones;
+
+    this.nombreParque = list.Nombre;
+
+    this.fk_idParque = list.Id;
+
+    this.loadingParks = false;
+
+    this.loadCedula();
+  }
+
+  saveCedula(a: any): void {
+    let name = a.target.value;
+
+    this.cedula = name;
+
+    let list = this.listAdmins.filter(x => x.cedula === name)[0];
+    this.nombreCedula = list.nombre + " " + list.apellido;
+    //this.toastr.success("Has seleccionado la cedula:" + this.cedula, "Correcto");
+
+    this.loadingCedula = false;
+
+    this.loadingParks = true;
+
+  }
+
+  loadForm() {
+    this.entradaForm = new FormGroup({
+      id: new FormControl(this.entrada.id),
+      //   fecha: new FormControl(this.entrada.fecha, [Validators.required]),
+      estado: new FormControl(this.entrada.estado, [Validators.required]),
+      fechaVencimiento: new FormControl(this.entrada.fechaVencimiento, [Validators.required])
+    });
+  }
+
+  loadCedula() {
+
+    this.loadingCedula = true;
+    this.userService.getViewAdministradores().subscribe((res: Administrador[]) => {
+
+      this.listAdmins = res;
+
+      console.log(this.listAdmins);
+
+    });
+  }
+
+  loadParkes() {
+
+    this.loadingParks = true;
+
+    this.parkService.getParkNationals().subscribe((res: parkNational[]) => {
+      this.listParks = res;
+
+    })
+  }
+
+
   agregar() {
 
     console.log(this.entradaForm.value.id);
+
+    this.toastr.info("Espera", "Guardando...");
 
     if (this.entradaForm.value.id == "") {
 
@@ -97,6 +158,10 @@ export class EntradaComponent implements OnInit {
         if (res.response.status == "ok") {
           this.toastr.success("Se guardo la entrada", "Correcto.");
           this.ngOnInit();
+          this.nombreCedula = "";
+          this.nombreParque = "";
+          this.cedula = "";
+          this.fk_idParque = "";
         } else {
           this.toastr.error("Error al guardar la entrada", "Error.");
         }
@@ -110,8 +175,8 @@ export class EntradaComponent implements OnInit {
       this.setEntradaWithForm();
 
       this.entradaService.updateEntrada(this.entrada).subscribe((res: any) => {
-        alert("Hubo respuesta");
-        console.log(res);
+
+
       });
 
       this.cleanEntrada();
@@ -136,21 +201,26 @@ export class EntradaComponent implements OnInit {
 
   }
 
-  loadParkes() {
 
-    this.parkService.getParkNationals().subscribe((res: parkNational[]) => {
-      this.listParks = res;
-    })
-  }
 
 
 
   eliminar(Id: any) {
 
-    this.entradaService.eliminarEntrada(Id).subscribe((res: any) => {
-      alert("Hubo respuesta");
-      console.log(res);
-    });
+    this.getEntradaById(Id);
+
+    if (this.entrada.estado != "Cancelada") {
+
+      this.loading = true;
+
+      this.entradaService.eliminarEntrada(Id).subscribe((res: any) => {
+        this.ngOnInit();
+      });
+    } else {
+
+      this.toastr.error("Entrada ya cancelada.", "Error.");
+
+    }
   }
 
   getEntrada() {
@@ -159,9 +229,9 @@ export class EntradaComponent implements OnInit {
 
       this.loading = false
       this.listEntradas = res;
-      console.log(res);
 
     });
+
   }
 
   cleanEntrada() {
@@ -182,7 +252,7 @@ export class EntradaComponent implements OnInit {
     if (this.fk_idParque + "" != "") {
       this.entrada.fecha = this.entradaForm.value.fecha + "";
       this.entrada.fk_idParque = this.fk_idParque + "";
-      this.entrada.fk_cedula = this.entradaForm.value.fk_cedula + "";
+      this.entrada.fk_cedula = this.cedula + "";
       this.entrada.estado = this.entradaForm.value.estado + "";
       this.entrada.fechaVencimiento = this.entradaForm.value.fechaVencimiento + "";
       this.entrada.tarifa = this.tarifa + "";
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,6 +6,7 @@ import { User } from 'src/app/interface/user';
 import Swal from 'sweetalert2'
 import { ToastrService } from 'ngx-toastr';
 import { AlertService } from 'src/app/utils/alert.service';
+import { Padron, UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -17,15 +18,54 @@ export class RegisterComponent implements OnInit {
 
   loading: boolean = false;
   confirmPassword: string = "";
+  cedula: string = "";
+  padron: Padron = { apellido1: "", apellido2: "", cedula: "", nombre: "" }
   usuario: User = { id: "", nombre: '', apellido: '', correo: "", contrasena: "", salt: '', tipo: "" };
 
   registerForm: FormGroup = new FormGroup({});
 
   ngOnInit(): void {
-    this.clearForm();
+    this.Toastr.info("La cedula no debe tener espacios, ni separadores.", "Nacional.")
+    this.Toastr.info("La Ingresar el pasaporte.", "Extranjero.")
+    this.loadForm();
   }
 
-  constructor(private LoginConnection: LoginConnectionService, private router: Router, private Toastr: ToastrService) { }
+  constructor(private LoginConnection: LoginConnectionService, private userService: UserService, private router: Router, private Toastr: ToastrService) { }
+
+  cargarData(e: any): void {
+
+
+
+    let cedula = e.target.value;
+
+    if (parseInt(cedula)) {
+      this.userService.getPadron(cedula).subscribe((res: any) => {
+
+        this.padron = res[0];
+
+        if (res[0] != undefined) {
+
+          this.loadForm();
+
+        } else {
+          this.Toastr.error("La cedula no fue encontrada en el Padron.", "Error Persona no encontrada");
+        }
+      });
+    } else {
+      this.Toastr.info("En caso de ser Correcto ignore este mensaje.", "Se detectaron caracteres de Pasaporte");
+    }
+
+  }
+
+  loadForm() {
+    this.registerForm = new FormGroup({
+      cedula: new FormControl(this.padron.cedula, Validators.required),
+      nombre: new FormControl(this.padron.nombre, Validators.required),
+      apellido: new FormControl(this.padron.apellido1, Validators.required),
+      correo: new FormControl("", [Validators.required]),
+      contrasena: new FormControl("", [Validators.required])
+    });
+  }
 
   clearForm() {
     this.registerForm = new FormGroup({
