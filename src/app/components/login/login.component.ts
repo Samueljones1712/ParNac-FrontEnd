2cb3b6107b71bf2bb9acf75e707e94ac6a834b98
XFgl <89269447+XFgl@users.noreply.github.com>
2023-05-27 00:51:14 -0600

    CSS tablas e íconos

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -40,9 +40,7 @@ export class LoginComponent implements OnInit {
     // Acceder a los valores de los campos
     const email_expresion = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
     const password_expresion = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[A-Z]).{8,}$/;
-    if () {
 
-    }
     if (this.loginForm.value.correo == '' || this.loginForm.value.password == '') {
 
       this.toastr.error('Todos los campos son obligatorios', 'Error');
